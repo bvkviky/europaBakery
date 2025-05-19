e3fa28ba7319d601e93b0b1a66000037d30011fa
vbocsard <viktoria.katalin.bocsardi@mindflytech.com>
2025-05-19 17:44:26 +0200

    navbar, homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {NavbarComponent} from "./shared/component/navbar/navbar.component";
+import {HomeComponent} from "./feature/home/home.component";
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, NavbarComponent, HomeComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
