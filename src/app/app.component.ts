b81cd90bdfaadc5c6b090d408494ccbce3632ebe
vbocsard <viktoria.katalin.bocsardi@mindflytech.com>
2025-05-19 18:27:23 +0200

    all pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {NavbarComponent} from "./shared/component/navbar/navbar.component";
 import {HomeComponent} from "./feature/home/home.component";
+import {GaleriaComponent} from "./feature/galeria/galeria.component";
+import {AboutusComponent} from "./feature/aboutus/aboutus.component";
+import {ContactComponent} from "./feature/contact/contact.component";
 
 @Component({
-  imports: [RouterModule, NavbarComponent, HomeComponent],
+  imports: [RouterModule, NavbarComponent, HomeComponent, GaleriaComponent, AboutusComponent, ContactComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
