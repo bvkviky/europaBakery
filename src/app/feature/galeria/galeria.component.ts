1d4c18185cc59068e1b2673bc097421ed47980b6
vbocsard <viktoria.katalin.bocsardi@mindflytech.com>
2025-05-21 12:08:03 +0200

    carusel#2 and all

--- src/app/feature/galeria/galeria.component.ts
+++ src/app/feature/galeria/galeria.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Carousel} from "primeng/carousel";
+import {PrimeTemplate} from "primeng/api";
 
 @Component({
   selector: 'app-galeria',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, Carousel, PrimeTemplate],
   templateUrl: './galeria.component.html',
-  styleUrl: './galeria.component.css',
+  styleUrls: ['./galeria.component.css'],
 })
-export class GaleriaComponent {}
+export class GaleriaComponent {
+  title = 'GFG';
+
+  images= [
+    {
+      previewImageSrc:
+          'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210203171024/CSSTutorial.png',
+      thumbnailImageSrc:
+          'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210203171024/CSSTutorial.png',
+      alt: 'Cascading Style Sheet',
+      title: 'CSS'
+    },
+    {
+      previewImageSrc:
+          'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210322182256/AngularJS-Tutorial.png',
+      thumbnailImageSrc:
+          'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210322182256/AngularJS-Tutorial.png',
+      alt: 'Angular for Front end',
+      title: 'Angular'
+    },
+    {
+      previewImageSrc:
+          'https://media.geeksforgeeks.org/wp-content/cdn-uploads/Java.png',
+      thumbnailImageSrc:
+          'https://media.geeksforgeeks.org/wp-content/cdn-uploads/Java.png',
+      alt: 'Java Programming Language',
+      title: 'Java'
+    },
+    {
+      previewImageSrc:
+          'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20220401124017/HTML-Tutorial.png',
+      thumbnailImageSrc:
+          'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20220401124017/HTML-Tutorial.png',
+      alt: 'HyperText Markup Language',
+      title: 'HTML'
+    },
+  ];
+}
